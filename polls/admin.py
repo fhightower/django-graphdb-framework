bc126b656660371e90c9f4842fe0bcdbcd8dc922
fhightower <floyd.hightower27@gmail.com>
2017-01-16 17:35:37 -0400

    Added the final updates from the last part of the tutorial

--- polls/admin.py
+++ polls/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 
 from .models import Question
 
-admin.site.register(Question)
 
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+
+admin.site.register(Question, QuestionAdmin)
